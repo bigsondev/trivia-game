da293f1a2c82152ddac5ee6813ce6fb33d1772b2
Adrian Bigaj <adrian.bigaj@bigsondev.com>
2021-03-22 13:07:47 +0100

    Customize preset to work with jsx transform and add initial async/await code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react"
+
 
 export const App = () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean');
+            const data = await response.json();
+
+            console.log('DATA: ', data);
+        }
+
+        fetchData();
+    }, [])
+
     return <div>Hello new boilerplate</div>
 }
